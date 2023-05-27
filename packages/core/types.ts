86fc85ab8d4c28141586684b152e2d2810d3a751
cijiugechu <cijiugechu@gmail.com>
2023-05-27 23:29:17 +0800

    chore: update comment for options

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -2,8 +2,17 @@ import type { ProxyAgent } from 'undici'
 
 export interface DeepLXOptions {
   content: string
+  /**
+   * @default 'EN'   
+   */
   targetLang?: string
   sourceLang?: string
+  /**
+   * @default false
+   */
   needAlternative?: boolean
+  /**
+   * @default undefined
+   */
   proxy?: ProxyAgent.Options
 }
